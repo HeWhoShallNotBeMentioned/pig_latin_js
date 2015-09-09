17b13a76a212911d44074d57d18048190e1fddf4
Corrie Smith & Chris Underwood <student@epicodus.com>
2015-09-09 12:00:26 -0700

    moves the consonants to the end of a word and adds 'ay' if the word starts with two to three consonants

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,28 @@ var pigLatin = function(word) {
   var word1 = word
   var vowels = ["a","e","i","o","u"];
   var firstLetter = word1.charAt(0);
+  var secondLetter = word1.charAt(1);
+  var thirdLetter = word1.charAt(2);
   var ipsy = vowels.indexOf(firstLetter);
+  var ipsy2 = vowels.indexOf(secondLetter);
+  var ipsy3 = vowels.indexOf(thirdLetter);
 
     if (ipsy >= 0) {
       return word1 = word1 + "ay";
     }
-    else {
+    else if (ipsy2 >= 0) {
       word1 = word1.slice(1);
       return word1 + firstLetter + "ay";
+    }
+
+    else if (ipsy3 >= 0) {
+      word1 = word1.slice(2);
+      return word1 + firstLetter + secondLetter + "ay";
+        }
+
+    else {
+      word1 = word1.slice(3);
+      return word1 + firstLetter + secondLetter + thirdLetter + "ay";
     }
 
 };
--- spec/specs.js
+++ spec/specs.js
@@ -6,4 +6,12 @@ describe('pigLatin', function () {
   it("moves the consonant to the end of a word and adds 'ay' if a word starts with one consonant", function () {
     expect(pigLatin("search")).to.eql("earchsay");
   });
+
+  it("moves the consonants to the end of a word and adds 'ay' if a word starts with two consonants", function () {
+    expect(pigLatin("scared")).to.eql("aredscay");
+  });
+
+    it("moves the consonants to the end of a word and adds 'ay' if a word starts with three consonants", function () {
+      expect(pigLatin("chris")).to.eql("ischray");
+  });
 });
