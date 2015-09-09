d26eef9e2be6e6640b36a6442218a2b98f694467
Corrie Smith & Chris Underwood <student@epicodus.com>
2015-09-09 11:11:47 -0700

    front end routing will run

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,23 @@ var pigLatin = function(word) {
   var vowels = ["a","e","i","o","u"];
   var firstLetter = word1.charAt(0);
   var ipsy = vowels.indexOf(firstLetter);
-  
+
     if (ipsy >= 0) {
       return word1 = word1 + "ay";
     }
 
 
 };
+
+$(document).ready(function() {
+  $("form#pig-latin").submit(function(event) {
+    var word = $("input#word").val();
+    var result = pigLatin(word);
+
+  $(".word").text(word);
+  $(".result").text(result);
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
