869c2049fa3a821f084a3f68e1de778bbed6429a
Corrie Smith & Chris Underwood <student@epicodus.com>
2015-09-09 17:11:58 -0700

    adds code and specs to allow end user to enter a sentence and have it returned in pig latin

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,53 @@
 var pigLatin = function(word) {
+  var wordSalad = word.split(" ");
+  var word1 ="";
+  var userOutput = "";
 
-  var word1 = word
-  var vowels = ["a","e","i","o","u"];
-  var firstLetter = word1.charAt(0);
-  var secondLetter = word1.charAt(1);
-  var thirdLetter = word1.charAt(2);
-  var ipsy = vowels.indexOf(firstLetter);
-  var ipsy2 = vowels.indexOf(secondLetter);
-  var ipsy3 = vowels.indexOf(thirdLetter);
+
+wordSalad.forEach(function(word){
+    word1 = word;
+    var vowels = ["a","e","i","o","u"];
+    var firstLetter = word1.charAt(0);
+    var secondLetter = word1.charAt(1);
+    var thirdLetter = word1.charAt(2);
+    var fourthLetter = word1.charAt(3);
+    var ipsy = vowels.indexOf(firstLetter);
+    var ipsy2 = vowels.indexOf(secondLetter);
+    var ipsy3 = vowels.indexOf(thirdLetter);
+    if (userOutput != "") {
+      userOutput = userOutput + " "
+    }
 
     if (ipsy >= 0) {
-      return word1 = word1 + "ay";
+      userOutput =  userOutput + word1 + "ay";
+      //return userOutput;
+    } else if (ipsy2 >= 0) {
+      if (firstLetter != "q") {
+        word1 = word1.slice(1);
+        userOutput = userOutput + word1 + firstLetter + "ay";
+        //return userOutput;
+      } else {
+          word1 = word1.slice(2);
+          userOutput = userOutput + word1 + firstLetter + secondLetter + "ay";
+          //return userOutput;
+      }
+    } else if (ipsy3 >= 0) {
+      if (secondLetter != "q") {
+          word1 = word1.slice(2);
+          userOutput = userOutput + word1 + firstLetter + secondLetter + "ay";
+          //return userOutput;
+      } else {
+          word1 = word1.slice(3);
+          userOutput = userOutput + word1 + firstLetter + secondLetter + thirdLetter + "ay";
+          //return userOutput;
+      }
+    } else {
+          word1 = word1.slice(3);
+          userOutput =  userOutput + word1 + firstLetter + secondLetter + thirdLetter + "ay";
+          //return userOutput;
     }
-    else if (ipsy2 >= 0) {
-      word1 = word1.slice(1);
-      return word1 + firstLetter + "ay";
-    }
-
-    else if (ipsy3 >= 0) {
-      word1 = word1.slice(2);
-      return word1 + firstLetter + secondLetter + "ay";
-        }
-
-    else {
-      word1 = word1.slice(3);
-      return word1 + firstLetter + secondLetter + thirdLetter + "ay";
-    }
-
+  });
+  return userOutput;
 };
 
 $(document).ready(function() {
--- spec/specs.js
+++ spec/specs.js
@@ -14,4 +14,12 @@ describe('pigLatin', function () {
     it("moves the consonants to the end of a word and adds 'ay' if a word starts with three consonants", function () {
       expect(pigLatin("chris")).to.eql("ischray");
   });
+
+  it("moves the letters 'q' and 'u' to the end of a given word and adds 'ay'", function () {
+    expect(pigLatin("quarter")).to.eql("arterquay");
+  });
+
+  it("takes a sentence and breaks it out into individual words for translation to pig latin", function () {
+    expect(pigLatin("baseball is fun")).to.eql("aseballbay isay unfay");
+  });
 });
